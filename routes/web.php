b3e5b613dee5a3e0a3db1a9972b09fdcdb681016
gu17 <gustavopantoja.ap@gmail.com>
2023-08-12 23:17:23 -0300

    inicial

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect(url('admin/login'));
 });
+Route::get('/home', function () {
+    return redirect(url('admin/dashboard'));
+});
